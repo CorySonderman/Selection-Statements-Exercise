f714c6e9f0ba1aa1f522c2bc3af42de6dfdb54c7
CorySonderman <sonderman2000@yahoo.com>
2023-10-02 07:40:59 -0500

    completed Selection Statement Exercis 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Let\'s see it you can guess my favorite number.");
+            Console.WriteLine("Pick a number and I will tell you if you are high " +
+                "or low.");
+            Console.WriteLine();
+            var guess = int.Parse (Console.ReadLine());
+            var r = new Random();
+            var favNumber = r.Next(1,1000);
+            if (guess < favNumber) 
+            {
+                Console.WriteLine("Too low.");
+            }
+            else if (guess > favNumber) 
+            {
+                Console.WriteLine("Too high");
+            }
+            else { Console.WriteLine("Bingo.  You win!");
+            }
         }
     }
 }
